982c814e479e61feb8c98829236fa06e84cc3ad6
Ihor Voitiuk <i.voitiuk@bluebisystems.nl>
2023-12-17 18:53:54 +0100

    Init base project

---
+++ src/components/Card.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div
+            class="relative bg-white border border-slate-100 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl">
+            <img src="/like-1.svg" alt="Like 1" class="absolute top-0 left-8">
+            <img src="/sneakers/sneakers-1.jpg" alt="Sneaker">
+            <p class="mt-2">Men`s sneakers Nike Blazer Mind Suede</p>
+
+            <div class="flex justify-between mt-5">
+                <div class="flex flex-col">
+                    <span class="text-slate-400">Price:</span>
+                    <b>$ 40</b>
+                </div>
+
+                <div>
+                    <input type="image" src="/plus.svg" alt="Plus">
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <header class="flex justify-between border-b border-slate-200 px-10 py-8">
+
+        <div class="flex items-center gap-4">
+            <img src="/logo.png" alt="Logo" class="w-10">
+            <div>
+                <h2 class="text-xl font-bold uppercase">Vue Sneakers</h2>
+                <p class="text-slate-500">The best sneaker store</p>
+            </div>
+        </div>
+
+        <ul class="flex item-center gap-10">
+            <li class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black">
+                <img src="/cart.svg" alt="Cart">
+                <b>$ 40</b>
+            </li>
+
+            <li class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black">
+                <img src="/heart.svg" alt="Bookmarks">
+                <span>Bookmarks</span>
+            </li>
+
+            <li class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black">
+                <img src="/profile.svg" alt="Profile">
+                <span>Profile</span>
+            </li>
+        </ul>
+    </header>
+</template>
